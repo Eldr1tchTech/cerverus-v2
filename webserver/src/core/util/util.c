43e41f053390f52bc18c2a98e9a97ed2c198e882
Eldr1tchTech <eldr1tchtech.dev@gmail.com>
2026-03-14 15:54:44 -0600

    now fixed the infinite loops?

--- webserver/src/core/util/util.c
+++ webserver/src/core/util/util.c
@@ -6,7 +6,8 @@
 #include <stdarg.h>
 #include <stdio.h>
 
-char* asprintf(char* fmt, ...) {
+char *asprintf(char *fmt, ...)
+{
     va_list args;
     va_start(args, fmt);
 
@@ -15,11 +16,12 @@ char* asprintf(char* fmt, ...) {
     int needed = vsnprintf(NULL, 0, fmt, args_copy);
     va_end(args_copy);
 
-    if (needed < 0) {
+    if (needed < 0)
+    {
         LOG_DEBUG("asprintf - Tried to return value of length 0.");
     }
 
-    char* str = cmem_alloc(memory_tag_string, needed + 1);
+    char *str = cmem_alloc(memory_tag_string, needed + 1);
     if (str)
     {
         vsnprintf(str, needed + 1, fmt, args);
@@ -30,7 +32,8 @@ char* asprintf(char* fmt, ...) {
 }
 
 // STRing CHaRacter Count
-int strchrc(char* str, char c) {
+int strchrc(char *str, char c)
+{
     int count = 0;
     while (*str != '\0')
     {
@@ -38,23 +41,26 @@ int strchrc(char* str, char c) {
         {
             count++;
         }
-        *str++;
+        str++;
     }
     return count;
 }
 
 // STRing CHaRacter Index
-int strchri(char* str, char c) {
+int strchri(char *str, char c)
+{
     int i = 0;
     while (*str != '\0')
     {
         if (*str == c)
         {
             return i;
-        } else {
+        }
+        else
+        {
             i++;
         }
-        *str++;
+        str++;
     }
     return -1;
 }
